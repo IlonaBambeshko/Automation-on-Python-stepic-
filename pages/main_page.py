f6f873e8408dc4bdc3ca663cdaeafca6f41971bc
Ilona <ilona_bambeshko@tut.by>
2021-04-22 21:52:10 +0300

    Refactor test for login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
-from .locators import LoginPageLocators
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
@@ -11,11 +11,4 @@ class MainPage(BasePage):
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
-    def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "It is not a login url"
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
