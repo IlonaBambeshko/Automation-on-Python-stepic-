6b60bbd068a9765f96ced2efb3e7bcc050e15a0e
Ilona <ilona_bambeshko@tut.by>
2021-04-22 19:31:27 +0300

    Expand test for user to click login link

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
